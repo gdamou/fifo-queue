cb79c6e4851a2a37627564f5ea0267e6618df520
Ghislain Damou <ghislain.damou@epsi.fr>
2023-12-07 16:40:30 +0100

    feat/backend: cors + send actual queueStatus on ws connection

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { queueRouter } from './routes/queueRouter';
+import cors from 'cors';
 
 const app = express();
 
 // Middlewares
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use('/queue', queueRouter);
--- packages/backend/src/services/websocketService.ts
+++ packages/backend/src/services/websocketService.ts
@@ -1,9 +1,21 @@
-import { WebSocketServer } from 'ws';
+import { WebSocket, WebSocketServer } from 'ws';
+import { getQueueStatus } from './queueService';
 
 let wss: WebSocketServer;
 
 export const initWebSocketServer = (httpWss: any) => {
     wss = new WebSocketServer({ server: httpWss });
+
+    wss.on('connection', async (ws) => {
+        // Fetch the current queue status
+        const queueStatus = await getQueueStatus();
+
+        // Convert the queue status to a string (e.g., JSON) if it's not already
+        const queueStatusString = JSON.stringify(queueStatus);
+
+        // Send the queue status to the connected client
+        ws.send(queueStatusString);
+    });
 };
 
 export const sendToWebSocket = (message: string) => {
